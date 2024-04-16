bf362e25eb5cdbe99d7bdb6d0692d3dc5b6e973e
Joaquinfer7688 <7688@holbertonstudents.com>
2024-04-16 16:48:26 -0700

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+project api task 1
+"""
+import requests
+import sys
+import csv
+
+
+def get_employee(user_id):
+    """
+    Obtains and displays information about an employee's tasks.
+    """
+    user_id = sys.argv[1]
+    user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
+        user_id)
+    name = requests.get(user).json().get("name")
+    request_todo = requests.get(todos).json()
+
+    filename = "{}.csv".format(user_id)
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        csv_format = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
+    for task in request_todo:
+        csv_format.append([user_id, name, task.get("completed"), task.get("title")])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        get_employee(sys.argv[1])
